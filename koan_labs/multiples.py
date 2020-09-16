45f93593a81adbba84a68b73e8d436c51ba8d647
sadatt254 <sadatt254@gmail.com>
2020-09-16 16:21:58 +0300

    completed the multiples activities

--- koan_labs/multiples.py
+++ koan_labs/multiples.py
@@ -1,4 +1,10 @@
 def multiple_sum():
-	#TODO: Your code goes here
-	return
+	
+	thao = range(1, 1000)
+	lst = []
+	for i in thao:
+		if i % 3 == 0 or i % 5 == 0:
+			lst.append(i)
+
+	return sum(i for i in lst)
 	
